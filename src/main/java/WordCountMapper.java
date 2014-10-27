684b9636a587902b4455fb3978c4566e3bed8c50
Scott Davis <scott.davis@webtrends.com>
2014-10-27 12:14:20 -0700

    initial commit for word counter example.

---
+++ src/main/java/WordCountMapper.java
@@ -0,0 +1,25 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+/**
+ * Created by daviss on 10/27/14.
+ */
+public class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context
+    ) throws IOException, InterruptedException {
+        StringTokenizer itr = new StringTokenizer(value.toString());
+        while (itr.hasMoreTokens()) {
+            word.set(itr.nextToken());
+            context.write(word, one);
+        }
+    }
+
+}
