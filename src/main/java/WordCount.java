a351bcb0969a2c06d5d925de5f679fb0676412d0
Scott Davis <scott.davis@webtrends.com>
2014-10-28 10:29:05 -0700

    added log4j configuration.

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -9,14 +9,21 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 public class WordCount {
 
+    private static final Logger logger = Logger.getLogger(WordCount.class);
 
     public static void main(String[] args) throws Exception {
 
-        Configuration conf = new Configuration();
 
+        BasicConfigurator.configure();
+
+        logger.info("Running Wordcount");
+
+        Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "word count");
 
         job.setJarByClass(WordCount.class);
@@ -31,6 +38,8 @@ public class WordCount {
         FileInputFormat.addInputPath(job, new Path("input"));
         FileOutputFormat.setOutputPath(job, new Path("output"));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+        logger.info("Completed.");
 
     }
 }
